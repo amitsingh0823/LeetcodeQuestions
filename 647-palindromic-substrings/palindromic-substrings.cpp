0c6adc597c8791421252c48922329e0bd07993af
Amit Singh <amit.singh0823@gmail.com>
2026-07-09 23:26:06 +0530

    Time: 43 ms (22.55%) | Memory: 34.7 MB (12.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int ans = 0;
-
-    // -1 = not computed
-    //  0 = false
-    //  1 = true
     vector<vector<int>> dp;
 
+    // Recursive + Memoization
     bool isPalindrome(string &s, int left, int right)
     {
-        // Base case
+        // Base Case
         if (left >= right)
             return true;
 
-        // Already computed
+        // Already Computed
         if (dp[left][right] != -1)
             return dp[left][right];
 
@@ -21,40 +17,27 @@ public:
         if (s[left] != s[right])
             return dp[left][right] = false;
 
-        // Recurse for inner substring
+        // Check inner substring
         return dp[left][right] = isPalindrome(s, left + 1, right - 1);
-    }
-
-    // Recursively replace inner loop
-    void solveEnd(string &s, int start, int end)
-    {
-        if (end == s.size())
-            return;
-
-        if (isPalindrome(s, start, end))
-            ans++;
-
-        solveEnd(s, start, end + 1);
-    }
-
-    // Recursively replace outer loop
-    void solveStart(string &s, int start)
-    {
-        if (start == s.size())
-            return;
-
-        solveEnd(s, start, start);
-
-        solveStart(s, start + 1);
     }
 
     int countSubstrings(string s)
     {
         int n = s.size();
+        int ans = 0;
 
         dp.assign(n, vector<int>(n, -1));
 
-        solveStart(s, 0);
+        // Iterate over all possible starting indices
+        for (int start = 0; start < n; start++)
+        {
+            // Iterate over all possible ending indices
+            for (int end = start; end < n; end++)
+            {
+                if (isPalindrome(s, start, end))
+                    ans++;
+            }
+        }
 
         return ans;
     }
